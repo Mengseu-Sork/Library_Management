f791f463bfb430f509fc1fa7c394f94d36bb8c5a
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-05-29 14:04:12 +0700

    feat(complete Login and register featuer)

--- src/class/Member.ts
+++ src/class/Member.ts
@@ -1,13 +1,13 @@
-class Member {
-  constructor(
-    public memberID: string,
-    public firstName: string,
-    public lastName: string,
-    public email: string,
-    public phoneNumber: string
-  ) {}
+// class Member {
+//   constructor(
+//     public memberID: string,
+//     public firstName: string,
+//     public lastName: string,
+//     public email: string,
+//     public phoneNumber: string
+//   ) {}
 
-  viewBorrow() {}
-  reviewBook(book: Book, rating: number, comment: string) {}
-  returnBook(book: Book) {}
-}
+//   viewBorrow() {}
+//   reviewBook(book: Book, rating: number, comment: string) {}
+//   returnBook(book: Book) {}
+// }
