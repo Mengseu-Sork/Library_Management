a067bc93ab604180acaaa7af6aa094083fe8c0c2
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-06-02 10:04:35 +0700

    add address in Register

--- src/main.ts
+++ src/main.ts
@@ -6,51 +6,51 @@
 
 
 //  existingUsers
-const existingUsers: Register[] = [
-    new Register("John", "Doe", "john@gmail.com", "0123", 25, "pass"),
-    new Register("Jane", "Smith", "jane@gmail.com", "0456", 28, "word"),
-     new Register("Alice", "Smith", "alice@example.com", "0123", 22, "pass123"),
-    new Register("Bob", "Jones", "bob@example.com", "0456", 30, "pass456")
-];
+// const existingUsers: Register[] = [
+//     new Register("John", "Doe", "john@gmail.com", "0123", 25, "pass"),
+//     new Register("Jane", "Smith", "jane@gmail.com", "0456", 28, "word"),
+//      new Register("Alice", "Smith", "alice@example.com", "0123", 22, "pass123"),
+//     new Register("Bob", "Jones", "bob@example.com", "0456", 30, "pass456")
+// ];
 
 
 
-//  Register New user
-const newUser = new Register("Samon", "Suon", "joh@gmail.com", "0965", 19, "123456");
+// //  Register New user
+// const newUser = new Register("Samon", "Suon", "joh@gmail.com", "0965", 19, "123456");
 
-if (Register.checkEmail(newUser, existingUsers)) {
-    console.log(" Email is available");
-} else {
-    console.log(" Email already used");
-}
+// if (Register.checkEmail(newUser, existingUsers)) {
+//     console.log(" Email is available");
+// } else {
+//     console.log(" Email already used");
+// }
 
-// Login user
-const loginUser = new Login("aLice@example.com", "pass123");
-if (Login.check(loginUser, existingUsers)) {
-    console.log(" Login successful");
-} else {
-    console.log(" Invalid email or password");
-}
+// // Login user
+// const loginUser = new Login("aLice@example.com", "pass123");
+// if (Login.check(loginUser, existingUsers)) {
+//     console.log(" Login successful");
+// } else {
+//     console.log(" Invalid email or password");
+// }
 
 
-// Create BookID objects
+// // Create BookID objects
 
-// Create Book object
-const book = new Book(
-  "AA1",
-  "The Pragmatic Programmer", // BookName
-  "Non-Fiction",              // BookType
-  "Andy Hunt & Dave Thomas",  // Author
-  "Programming",              // Category
-  1999,                       // CreateYear
-  5                           // AvailableNumber
-)
-;
+// // Create Book object
+// const book = new Book(
+//   "AA1",
+//   "The Pragmatic Programmer", // BookName
+//   "Non-Fiction",              // BookType
+//   "Andy Hunt & Dave Thomas",  // Author
+//   "Programming",              // Category
+//   1999,                       // CreateYear
+//   5                           // AvailableNumber
+// )
+// ;
 
-// Display book info
-console.log(" Book Name:", book.getBookName());
-console.log(" Book IDs:", book.getBookIDs());
-console.log(" Author:", book.getAuthor());
-console.log(" Category:", book.getCategory());
-console.log(" Year:", book.getCreateYear());
-console.log(" Available Copies:", book.getAvailableNumber());
+// // Display book info
+// console.log(" Book Name:", book.getBookName());
+// console.log(" Book IDs:", book.getBookIDs());
+// console.log(" Author:", book.getAuthor());
+// console.log(" Category:", book.getCategory());
+// console.log(" Year:", book.getCreateYear());
+// console.log(" Available Copies:", book.getAvailableNumber());
