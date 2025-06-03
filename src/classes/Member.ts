1146433fa2b997e66d7e5fab0ed430ccb522b154
Mengseu Sork <mengseu.sork212004@gmail.com>
2025-06-03 16:21:30 +0700

    update file member from #7

--- src/classes/Member.ts
+++ src/classes/Member.ts
@@ -67,16 +67,18 @@ export class Member extends Register {
     }
 
     public reserveBook(book: Book, bookingDate: Date): Booking {
-      if (!book.isAvailable()) {
-        console.log(`Book "${book.getBookTitle()}" is not available for booking.`);
-        throw new Error("Book not available");
-      }
+    if (!book.isAvailable()) {
+        const title = book.getBookTitle();
+        console.log(`Book "${title}" is not available for booking.`);
+        throw new Error(`Book "${title}" is not available for reservation.`);
+    }
 
-      let newBooking = new Booking(`BOOKING-${Date.now()}`, book, this, bookingDate);
-      this.bookedBooks.push(newBooking);
-      book.borrowCopy();
-      console.log(`Book "${book.getBookTitle()}" reserved by ${this.getFirstName()}.`);
-      return newBooking;
+    const newBooking = new Booking(`BOOKING-${Date.now()}`, book, this, bookingDate);
+    this.bookedBooks.push(newBooking);
+    book.borrowCopy();
+
+    console.log(`Book "${book.getBookTitle()}" reserved by ${this.getFirstName()}.`);
+    return newBooking;
     }
 
     public updateInfo(newAddress?: string, newPhone?: string): void {
